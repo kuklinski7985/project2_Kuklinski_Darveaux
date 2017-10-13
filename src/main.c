d04f4beaf45f340d4262f47e2e44c93bd85b0a3a
Andrew Kuklinski <31773850+kuklinski7985@users.noreply.github.com>
2017-10-13 12:37:16 -0600

    updated for project2

--- src/main.c
+++ src/main.c
@@ -2,18 +2,14 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "platform.h"
-#include "project1.h"
 #include "memory.h"
 #include "conversion.h"
 #include "debug.h"
-#include "feature1.h"
-#include <math.h>
 
 int main(){
   
- #ifdef PROJECT1
- project1();
- //feature1();
+ #ifdef PROJECT2
+ project2();
  #endif
  
   return 0;
