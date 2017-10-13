13c445744a654d395b6a0449f7871384b89e35d9
Andrew Kuklinski <31773850+kuklinski7985@users.noreply.github.com>
2017-10-13 12:35:59 -0600

    Adding files from project 1

---
+++ src/main.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include "platform.h"
+#include "project1.h"
+#include "memory.h"
+#include "conversion.h"
+#include "debug.h"
+#include "feature1.h"
+#include <math.h>
+
+int main(){
+  
+ #ifdef PROJECT1
+ project1();
+ //feature1();
+ #endif
+ 
+  return 0;
+}
+
+
+
+
