c295f4a005b1a1d813ab3893e9aa6339ee4e8e83
masondarv <robert.darveaux@colorado.edu>
2017-10-25 19:11:25 -0600

    data processing v1

--- src/project2.c
+++ src/project2.c
@@ -15,8 +15,85 @@
 #include "debug.h"
 #include "project2.h"
 
+uint8_t Alpha_count=0;
+uint8_t Num_count=0;
+uint8_t Punct_count=0;
+uint8_t Misc_count=0;
+uint8_t error_count=0;
 
-void project2(void){
+
+void project2(void)
+{
+
+
 
 
 }
+
+void process_Data(uint8_t data)
+{
+
+	if(65<=data<=90 || 97<=data<=122) 	//check if data is an alphabetical character in ASCII
+	Alpha_count++;
+
+	else if(48<=data<=57)			//check if data is a number in ASCII
+	Num_count++;
+
+	else if(32<=data<=47 || 58<=data<=64 || 91<=data<=96 || 123<=data<=126) // check if data is punctuation in ASCII
+	Punct_count=0;
+
+	else if(0<=data<=31 || data==127)
+	Misc_count++;
+	
+	else
+	error_count++;
+
+
+}
+
+void transmit_table()
+{
+	uint8_t Alpha[] = "# of Alphabetic Characters:  ";
+	uint16_t Alpha_Length = 29;
+	uint8_t Num[] = "# of Numeric Characters:  ";
+	uint16_t Num_Length = 26;
+	uint8_t Punct[] = "# of Punctuation Characters:  ";
+	uint16_t Punct_length = 29;
+	uint8_t Misc[] = "# of Miscellaneous Characters:  ";
+	uint16_t Misc_length = 32;
+
+	UART_send_n(Alpha, Alpha_Length);
+
+	UART_send(&Alpha_count);
+
+	
+	UART_send_n(Num, Num_Length);
+
+	UART_send(&Num_count);
+
+	
+	UART_send_n(Punct, punct_Length);
+
+	UART_send(&Punct_count);
+
+	
+	UART_send_n(Misc, Misc_Length);
+
+	UART_send(&Misc_count);
+
+}
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
